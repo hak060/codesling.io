8fdddcca410783093e919284637e6aaa878cdc9a
balex8888 <balex8888@gmail.com>
2017-11-14 17:04:27 -0800

    git update

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -7,7 +7,7 @@ import Auth from '../Auth';
 import InvalidSlingError from '../Sling/InvalidSlingError';
 import Protected from '../globals/Protected';
 
-import './App.css';
+import './App.css'; 
 
 const App = () => {
   return (
